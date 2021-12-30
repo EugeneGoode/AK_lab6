b456bc7064e95e1b05dd80b0fc6dd3494e47ed3b
EugeneGoode <66566549+EugeneGoode@users.noreply.github.com>
2021-12-30 02:41:49 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -10,10 +10,10 @@ MODULE_AUTHOR("Zhenya Hudenko <zhenyahudenko@gmail.com>");
 MODULE_DESCRIPTION("Lab 6");
 MODULE_LICENSE("Dual BSD/GPL");
 
-static uint count = 1;
+static uint counter = 1;
 
-module_param(count, uint, 0444);
-MODULE_PARM_DESC(count, "Number of prints");
+module_param(counter, uint, 0444);
+MODULE_PARM_DESC(counter, "the number of hello world");
 
 struct my_struct {
 	struct list_head tlist;
@@ -24,20 +24,20 @@ static LIST_HEAD(my_list_head);
 
 static int __init hello_init(void)
 {	
-	if (count == 0) {
-		pr_warn("The parameter is 0");
-	} else if (count >= 5 && count <= 10) {
-		pr_warn("The parameter is in [5; 10]");
+	if (counter == 0) {
+		pr_warn("you cannot enter 0");
+	} else if (counter >= 5 && counter <= 10) {
+		pr_warn("The parameter is between 5 and 10");
 	}
 
-	BUG_ON((count > 10));
+	BUG_ON((counter > 10));
 
 	int i;
-	for (i = 0; i <= count; i++) {
+	for (i = 0; i <= counter; i++) {
 
 		struct my_struct *ms = NULL;
 
-		if (i != count)
+		if (i != counter)
 		{
 			ms = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
 		}
@@ -52,15 +52,15 @@ static int __init hello_init(void)
 
 static void __exit hello_exit(void)
 {
-	int count = 0;
+	int counter = 0;
 	struct my_struct *t, *tmp;
 
 	list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
-		pr_info("i: %d; ktime = %lld\n", count++, ktime_to_ns(t->time));
+		pr_info("i: %d; ktime = %lld\n", counter++, ktime_to_ns(t->time));
 		list_del(&t->tlist);
 		kfree(t);
 	}
-	pr_info("Module unloaded\n");
+	pr_info("Module removed\n");
 }
 
 module_init(hello_init);
